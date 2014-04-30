065efb713897111a2468ea9beb5695f5aacbd843
neymello <neymello@gmail.com>
2014-04-30 01:44:11 -0400

    Implement tsplit method on sqlite server component.

--- src/components/implementation/sqlite/sqlite_server.c
+++ src/components/implementation/sqlite/sqlite_server.c
@@ -13,18 +13,47 @@
 #include <cbuf.h>
 #include <evt.h>
 #include <torrent.h>
+#include <sqlite3.h>
 
-void cos_init(void)
+static long evt_sqlite = 0;
+static td_t TD_SQLITE = td_null;
+static sqlite3 *SQLITE_DB;
+
+
+void
+cos_init(void)
 {
 	printc("\n\n\n#SQLITE: initialization!!!\n\n\n");
 
 	return;
 }
 
-td_t tsplit(spdid_t spdid, td_t tid, char *param, int len, tor_flags_t tflags, long evtid){
-    printc("#SQLITE: tsplit ok\n");
+td_t
+tsplit(spdid_t spdid, td_t tid, char *param, int len, tor_flags_t tflags, long evtid)
+{
+    int rc;
+    struct torrent *nt;
 
-    return -ENOTSUP;
+    printc("#SQLITE: tsplit ok\n");
+    if (TD_SQLITE) {
+        return TD_SQLITE;
+    }
+
+    printc("#SQLITE: Creating the database\n");
+    rc = sqlite3_open_v2(":memory:", &SQLITE_DB, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_PRIVATECACHE, NULL);
+    if (rc) {
+        printf("#SQLITE: Can't open database (%d): %s\n", rc, sqlite3_errmsg(SQLITE_DB));
+        sqlite3_close(SQLITE_DB);
+        return -EAGAIN;
+    }
+
+    nt = tor_alloc(NULL, tflags);
+    if (!nt) ERR_THROW(-ENOMEM, done);
+    nt->evtid = evtid;
+
+    TD_SQLITE = nt->td;
+
+    return TD_SQLITE;
 }
 
 int treadp(spdid_t spdid, td_t td, int *off, int *sz){
