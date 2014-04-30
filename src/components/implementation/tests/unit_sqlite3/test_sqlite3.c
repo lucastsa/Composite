72643a9f649fddc38808d8183d78a704a25c4894
lucastsa <lucas.sa@gmail.com>
2014-04-30 02:30:07 -0400

    included SQLITE_MEMDEBUG directive and hardcoded randmoness in VFS to avoid memory leaks for now

--- src/components/implementation/tests/unit_sqlite3/test_sqlite3.c
+++ src/components/implementation/tests/unit_sqlite3/test_sqlite3.c
@@ -83,57 +83,57 @@ void cos_init(void) {
         return;
     }
 
-    // // creates table
-    // printf("#SQLITE: executing prepare for CREATE\n");
-    // // r = sqlite3_exec(db, CREATE_STMT, data_handler, (void*)CREATE_STMT, &errmsg);
-    // r = sqlite3_prepare_v2(db, CREATE_STMT, -1, &stmt, NULL);
-    // if (r) {
-    //     printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
-    //     return;
-    // }
+    // creates table
+    printf("#SQLITE: executing prepare for CREATE\n");
+    // r = sqlite3_exec(db, CREATE_STMT, data_handler, (void*)CREATE_STMT, &errmsg);
+    r = sqlite3_prepare_v2(db, CREATE_STMT, -1, &stmt, NULL);
+    if (r) {
+        printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
+        return;
+    }
 
-    // printf("#SQLITE: executing step for CREATE\n");
-    // r = sqlite3_step(stmt);
-    // if (r != SQLITE_DONE) {
-    //     printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
-    //     return;
-    // }
+    printf("#SQLITE: executing step for CREATE\n");
+    r = sqlite3_step(stmt);
+    if (r != SQLITE_DONE) {
+        printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
+        return;
+    }
 
-    // // preparing only once for the loop
-    // r = sqlite3_prepare_v2(db, INSERT_STMT, -1, &stmt, NULL);
-    // if (r) {
-    //     printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
-    //     return;
-    // }
+    // preparing only once for the loop
+    r = sqlite3_prepare_v2(db, INSERT_STMT, -1, &stmt, NULL);
+    if (r) {
+        printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
+        return;
+    }
 
-    // // inserting names
-    // for (i=0; i < N_NAMES; i++) {
-    //     r = sqlite3_bind_text(stmt, 1, names[i], strlen(names[i]), SQLITE_STATIC);
-    //     if (r)  {
-    //         printf("#SQLITE: Bind error: %d : %s\n", r, sqlite3_errmsg(db));
-    //         return;
-    //     }
+    // inserting names
+    for (i=0; i < N_NAMES; i++) {
+        r = sqlite3_bind_text(stmt, 1, names[i], strlen(names[i]), SQLITE_STATIC);
+        if (r)  {
+            printf("#SQLITE: Bind error: %d : %s\n", r, sqlite3_errmsg(db));
+            return;
+        }
 
-    //     r = sqlite3_step(stmt);
-    //     if (r != SQLITE_DONE) {
-    //         printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
-    //         return;
-    //     }
+        r = sqlite3_step(stmt);
+        if (r != SQLITE_DONE) {
+            printf("#SQLITE: SQL error: %d : %s\n", r, sqlite3_errmsg(db));
+            return;
+        }
 
-    //     // resetting the statement for next step
-    //     r = sqlite3_reset(stmt);
-    //     if (r)  {
-    //         printf("#SQLITE: Reset error: %d : %s\n", r, sqlite3_errmsg(db));
-    //         return;
-    //     }
-    // }
+        // resetting the statement for next step
+        r = sqlite3_reset(stmt);
+        if (r)  {
+            printf("#SQLITE: Reset error: %d : %s\n", r, sqlite3_errmsg(db));
+            return;
+        }
+    }
 
-    // // finalize the statement to release resources
-    // sqlite3_finalize(stmt);
+    // finalize the statement to release resources
+    sqlite3_finalize(stmt);
 
     // select values
-    r = sqlite3_exec(db, "SELECT DATE();", data_handler, (void*)SELECT_STMT, &errmsg);
-    // r = sqlite3_exec(db, SELECT_STMT, data_handler, (void*)SELECT_STMT, &errmsg);
+    // r = sqlite3_exec(db, "SELECT RANDOM();", data_handler, (void*)SELECT_STMT, &errmsg);
+    r = sqlite3_exec(db, SELECT_STMT, data_handler, (void*)SELECT_STMT, &errmsg);
     if (r) {
         printf("#SQLITE: SQL error: %s\n", errmsg);
         sqlite3_free(errmsg);
